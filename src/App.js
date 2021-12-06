05c44ec1e18bbe5df47e9bf6e4bc828e4fb5976f
Fatih <54095238+ahmadfatihin@users.noreply.github.com>
2021-12-06 10:36:11 +0700

    Creating a native stack navigator
    Add React Navigation Starter Template to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import * as React from 'react';
 import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-const App = () => {
+function HomeScreen() {
   return (
-    <View>
-      <Text>OnJob Ready To Go!</Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
     </View>
   );
-};
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;

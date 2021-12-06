7743ccf829011466a5d1e02f9993b3247fede1cd
Fatih <54095238+ahmadfatihin@users.noreply.github.com>
2021-12-06 14:03:07 +0700

    Set Splash Screen Timer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './screens/Home';
 import Splash from './screens/Splash';
 
@@ -11,8 +11,16 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Splash" component={Splash} options={{ headerShown=False }} />
-        <Stack.Screen name="Home" component={Home} options={{ headerShown=False }} />
+        <Stack.Screen
+          name="Splash"
+          component={Splash}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{headerShown: false}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/Splash/index.js
+++ src/screens/Splash/index.js
@@ -2,6 +2,12 @@ import React, {Component} from 'react';
 import {Text, StyleSheet, View} from 'react-native';
 
 export default class Splash extends Component {
+  componentDidMount() {
+    setTimeout(() => {
+      this.props.navigation.replace('Home');
+    }, 3000);
+  }
+
   render() {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>

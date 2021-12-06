0bffcfea35cfb031d83ecc2a9e22538b5d46e278
Fatih <54095238+ahmadfatihin@users.noreply.github.com>
2021-12-06 11:46:46 +0700

    Import HomeScreen and SplashScreen to App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,8 @@ import * as React from 'react';
 import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-function HomeScreen() {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import Home from './pages/Home';
+import Splash from './pages/Splash';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,7 +11,8 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );

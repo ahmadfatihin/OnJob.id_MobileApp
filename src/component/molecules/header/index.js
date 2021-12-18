54ab87cb3d4f7d464787936992768403ef889d35
Fatih <54095238+ahmadfatihin@users.noreply.github.com>
2021-12-18 22:46:42 +0700

    Create Header Component

---
+++ src/component/molecules/header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Header = () => {
+  return (
+    <View>
+      <Text>Header Component</Text>
+    </View>
+  );
+};
+
+export default Header;
+
+const styles = StyleSheet.create({});

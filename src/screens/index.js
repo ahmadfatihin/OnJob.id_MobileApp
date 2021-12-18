9c876ab217700762f1550383613e34e3a0eb70dd
Fatih <54095238+ahmadfatihin@users.noreply.github.com>
2021-12-18 22:34:20 +0700

    Create Register Screens

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,5 +2,6 @@ import Home from './Home';
 import Splash from './Splash';
 import Welcome from './Welcome';
 import Login from './Login';
+import Register from './Register';
 
-export {Home, Splash, Welcome, Login};
+export {Home, Splash, Welcome, Login, Register};

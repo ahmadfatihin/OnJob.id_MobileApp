86412ff54f9e26f1b7d497499b677621d15cb14f
Fatih <54095238+ahmadfatihin@users.noreply.github.com>
2021-12-26 22:46:54 +0700

    Export BottomNav Icon

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,5 +1,25 @@
 import ArrowLeft from './arrow-left.svg';
 import IconAddPhoto from './ic-add-photo.svg';
 import IconRemovePhoto from './ic-remove-photo.svg';
+import IconHome from './ic_home.svg';
+import IconHomeAktif from './ic_home_aktif.svg';
+import IconOrder from './ic_order.svg';
+import IconOrderAktif from './ic_order_aktif.svg';
+import IconProfile from './ic_user.svg';
+import IconProfileAktif from './ic_user_aktif.svg';
+import IconChat from './ic_chat.svg';
+import IconChatAktif from './ic_chat_aktif.svg';
 
-export {ArrowLeft, IconAddPhoto, IconRemovePhoto};
+export {
+  ArrowLeft,
+  IconAddPhoto,
+  IconRemovePhoto,
+  IconHome,
+  IconHomeAktif,
+  IconOrder,
+  IconOrderAktif,
+  IconProfile,
+  IconProfileAktif,
+  IconChat,
+  IconChatAktif,
+};

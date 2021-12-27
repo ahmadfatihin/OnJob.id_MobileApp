9fe98337f769eb131539fbfab0d5e063e3a438b0
Fatih <54095238+ahmadfatihin@users.noreply.github.com>
2021-12-27 09:00:47 +0700

    Header Screen Off

--- src/router/index.js
+++ src/router/index.js
@@ -20,10 +20,18 @@ const Tab = createBottomTabNavigator();
 function MainApp() {
   return (
     <Tab.Navigator tabBar={props => <BottomNav {...props} />}>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Order" component={Order} />
-      <Tab.Screen name="Chat" component={Chat} />
-      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Home" component={Home} options={{headerShown: false}} />
+      <Tab.Screen
+        name="Order"
+        component={Order}
+        options={{headerShown: false}}
+      />
+      <Tab.Screen name="Chat" component={Chat} options={{headerShown: false}} />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{headerShown: false}}
+      />
     </Tab.Navigator>
   );
 }
@@ -49,11 +57,6 @@ const Router = () => {
       <Stack.Screen
         name="Register"
         component={Register}
-        options={{headerShown: false}}
-      />
-      <Stack.Screen
-        name="Home"
-        component={Home}
         options={{headerShown: false}}
       />
       <Stack.Screen

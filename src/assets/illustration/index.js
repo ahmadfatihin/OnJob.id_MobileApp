705a1867b04f38436fe0549d6c65a9e503cecf0c
Fatih <54095238+ahmadfatihin@users.noreply.github.com>
2021-12-29 10:30:31 +0700

    Import Illustration for MenuItem

--- src/assets/illustration/index.js
+++ src/assets/illustration/index.js
@@ -2,5 +2,24 @@ import Onjob from './Onjob.svg';
 import Logo from './logo.svg';
 import OnjobWhite from './OnjobWhite.svg';
 import NullPhoto from './null-photo.png';
+import Angkatin from './angkatin.png';
+import Anterin from './anterin.png';
+import Beliin from './beliin.png';
+import Cariin from './cariin.png';
+import Packingin from './packingin.png';
+import Sebarin from './sebarin.png';
+import Tukerin from '/tukerin.png';
 
-export {Onjob, Logo, OnjobWhite, NullPhoto};
+export {
+  Onjob,
+  Logo,
+  OnjobWhite,
+  NullPhoto,
+  Angkatin,
+  Anterin,
+  Beliin,
+  Cariin,
+  Packingin,
+  Sebarin,
+  Tukerin,
+};

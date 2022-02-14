1248e2b853f012625bf147fa4c3159a972b403d5
Fatih <54095238+ahmadfatihin@users.noreply.github.com>
2022-02-14 15:38:45 +0700

    add DummyUser1

--- src/assets/dummy/index.js
+++ src/assets/dummy/index.js
@@ -1,4 +1,5 @@
 import DummyNews1 from './DummyNews1.png';
 import DummyNews2 from './DummyNews2.png';
+import DummyUser1 from './DummyUser1.jpg';
 
-export {DummyNews1, DummyNews2};
+export {DummyNews1, DummyNews2, DummyUser1};
